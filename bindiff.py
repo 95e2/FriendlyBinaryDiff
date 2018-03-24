b02fe82e87b7bcc8b126edcb4e8b8c794b6574e2
urain39 <hexiedeshijie@gmail.com>
2018-03-24 19:48:37 +0800

    Update.

--- bindiff.py
+++ bindiff.py
@@ -8,6 +8,11 @@ _SIZE_ERR = 3
 _CHUNK_SIZE = 16
 _FIRST_ONLY = False # or True
 
+NONE = '\033[0m'
+LIGHT_RED = '\033[31m'
+LIGHT_GREEN = '\033[32;1m'
+LIGHT_YELLOW = '\033[33;1m'
+
 class DiffFile:
 	def __init__(self, fp):
 		self.__seek = 0
@@ -43,6 +48,26 @@ def chunkToHexStr(chunk):
 		# Python 3.x
 		return ''.join(['%02X '%(x) for x in chunk])
 
+"""
+                              ADDRESS: 016D8590
+OLD <<<========================================
+7F 25 92 BB 0D 42 20 C2 16 6D A4 AC CF 89 61 37 
+===============^v=============^v===============
+4F 25 92 BB 0D 82 20 C2 16 6D B9 AC CF 89 61 37 
+========================================>>> NEW
+"""
+
+def getDiffStr(diffOffsets):
+	seek = 0
+	diffStr = ""
+	for offset in diffOffsets:
+		diffStr += (offset - seek) * "==="
+		diffStr += LIGHT_GREEN + "^" + \
+		    LIGHT_RED + "V" + NONE + "="
+		seek = offset + 1
+	diffStr += (_CHUNK_SIZE - diffOffsets[-1] - 1) * "==="
+	return diffStr
+
 def diffStart(old, new):
 	while True:
 		oldChunk = old.getChunk()
@@ -50,14 +75,22 @@ def diffStart(old, new):
 		if (not oldChunk or
 		       not newChunk):
 			break
-		if oldChunk != newChunk:
-			print(" "*30 + "ADDRESS: %08X"%(old.getAddress() or
-			                                new.getAddress() ) )
-			print("OLD <<<" + "="*40)
+		
+		diffOffsets = []
+		for i in range(len(zip(oldChunk, newChunk))):
+			if oldChunk[i] != newChunk[i]:
+				diffOffsets.append(i) # record offset.
+
+		# FIXME: The result is not accurate.
+		DISPLAY_LEN = (_CHUNK_SIZE * 2 + _CHUNK_SIZE / 2 + 1)
+		if len(diffOffsets) > 0:
+			print("ADDRESS: %08X, COUNT(%d)"%(old.getAddress() or
+			                                new.getAddress(), len(diffOffsets) ) )
+			print(LIGHT_GREEN + "OLD <<<" + "="*DISPLAY_LEN + NONE)
 			print(chunkToHexStr(oldChunk))
-			print("="*47)
+			print(getDiffStr(diffOffsets))
 			print(chunkToHexStr(newChunk))
-			print("="*40 + ">>> NEW\n")
+			print(LIGHT_YELLOW + "="*DISPLAY_LEN + ">>> NEW\n" + NONE)
 			if _FIRST_ONLY: break
 
 def main(argv):
